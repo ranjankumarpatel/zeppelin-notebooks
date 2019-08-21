9c42c0991cf9b50191ed0cc356cefdda1b0a42d3
Ranjan Kumar Patel <ranjankumarpatel@users.noreply.github.com>
2019-08-21 22:51:12 +0530

    Update zepplin-docker.sh

--- zepplin-docker.sh
+++ zepplin-docker.sh
@@ -1,1 +1,4 @@
 docker container run -dit --name zeppelin -p 8080:8080 -p 4040:4040 --rm -v $PWD/logs:/logs -v $PWD/notebook:/notebook -e ZEPPELIN_LOG_DIR='/logs' -e ZEPPELIN_NOTEBOOK_DIR='/notebook' --name zeppelin apache/zeppelin:0.8.1
+
+docker exec -it sandbox-hdp bash
+ambari-admin-password-reset
